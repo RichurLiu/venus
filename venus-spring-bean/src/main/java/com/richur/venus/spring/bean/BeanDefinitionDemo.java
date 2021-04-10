0d7f267cd0be09ab26e4ab6f6adaf263154d4136
刘磊 <liulei01@bilibili.com>
2021-04-10 08:40:49 +0800

    spring

--- venus-spring-bean/src/main/java/com/richur/venus/spring/bean/BeanDefinitionDemo.java
+++ venus-spring-bean/src/main/java/com/richur/venus/spring/bean/BeanDefinitionDemo.java
@@ -1,7 +1,24 @@
 package com.richur.venus.spring.bean;
 
+import com.richur.venus.spring.ioc.User;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+
 /**
  * @author richur
  */
 public class BeanDefinitionDemo {
+
+    public static void main(String[] args) {
+        // 1.通过BeanDefinitionBuilder 构建
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
+        // 属性设置
+        beanDefinitionBuilder.addPropertyValue("age", 100);
+        beanDefinitionBuilder.addPropertyValue("name", "riiiiiiicher");
+        // 获取 BeanDefinition 实例
+        AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
+
+        // 2. 通过AbstractBeanDefinition以及派生类
+
+    }
 }
